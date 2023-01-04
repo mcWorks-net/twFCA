a114d691f564081e14e1005e1b98232f81e3e9ae
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2023-01-04 14:36:29 +0800

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+const plugin = require('tailwindcss/plugin');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+
+
+      colors: {
+        primary : "#008542",
+        dark: "#262626",
+        accent : "#28951e",
+        accentYellow: "#daa625",
+        lightGray: "#f3f8f4",
+      },
+
+      fontSize: {
+        huge: 'clamp(2.4rem, 6vw, 5rem)',
+      },
+
+      fontFamily : {
+        poppins : ['poppins', 'san-serif'],
+        raleway : ['Raleway', 'san-serif']
+      }
+
+    },
+
+    container : {
+      center: true,
+      padding: "15px",
+    }
+  },
+  plugins: [
+    plugin(function({ addVariant }) {
+      addVariant('current', '&.active');
+  })
+
+  ],
+}
