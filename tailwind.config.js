461b31e3fa30608ff27306aaf72597412e855c40
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2023-01-09 14:36:09 +0800

    homePage renposive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,10 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
-
-
       colors: {
         primary : "#008542",
         dark: "#262626",
-        accent : "#28951e",
+        accent : "#28941e",
         accentYellow: "#daa625",
         lightGray: "#f3f8f4",
         clrGray: "#f2f2f2"
